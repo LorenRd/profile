5d895fb5f5de77dc2aaa4c8c7c9b097bb2e68748
Lorenzo Rondan <lorenzo.rondan.dominguez@hp.com>
2022-10-24 10:59:03 +0200

    Fixes in calls

--- components/SocialLink.js
+++ components/SocialLink.js
@@ -6,13 +6,14 @@ export default function SocialLink({
     title,
     icon,
 }) {
+    const gaEventTracker = useAnalyticsEventTracker('Social networks');
     return (
         <a
             href={href}
             target="_blank"
             rel="noopener"
             className="text-blue-300 hover:underline block h-6 my-4 mx-4"
-            onClick={useAnalyticsEventTracker(title)}
+            onClick={() => gaEventTracker(title)}
         >
             <FontAwesomeIcon
                 className="h-full"
